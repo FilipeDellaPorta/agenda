bf90f7f1c5b1b687d0b992ee47fd86e6ddbc8767
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-07-15 15:52:57 -0300

    updated

--- js/CarregarAgenda.js
+++
@@ -1,9 +0,0 @@
-import { atualizarCabecalho } from './AtualizarCabecalho.js';
-import { criarAgendaParaData } from './CriarAgenda.js';
-import { dataAtual } from './DataAtual.js';
-
-// Carrega a agenda de hoje inicialmente
-export function carregarAgenda() {
-  atualizarCabecalho(dataAtual);
-  criarAgendaParaData(dataAtual);
-}
--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-import { carregarAgenda } from './CarregarAgenda.js';
+import { atualizarCabecalho } from './AtualizarCabecalho.js';
+import { criarAgendaParaData } from './CriarAgenda.js';
 import { dataAtual } from './DataAtual.js';
 
 // Inicializar
 carregarAgenda();
+
+// Carrega a agenda de hoje inicialmente
+export function carregarAgenda() {
+  atualizarCabecalho(dataAtual);
+  criarAgendaParaData(dataAtual);
+}
 
 // Navegação
 const btnAnterior = document.getElementById('anterior');
